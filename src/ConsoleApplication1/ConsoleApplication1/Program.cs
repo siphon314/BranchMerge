99368a14c3401ca31e19673284ee17f44556ba6b
siphon <siphon314+git@gmail.com>
2013-05-18 21:26:28 +0900

    １秒おきに現在時刻を出力

--- src/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ src/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApplication1
@@ -10,6 +11,11 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine(DateTime.Now);
+                Thread.Sleep(1000);
+            }
         }
     }
 }
